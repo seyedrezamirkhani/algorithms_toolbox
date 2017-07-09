a18d86f307130df932bb9fb3a502f3b1ee0e26d7
Seyed Reza Mirkhani <reza@machinelearner.home>
2017-07-09 22:55:50 +0100

    Added code to implement Calculate LCM using Greatest Common Divisor(GCD)

--- 004_Programming_Assignment/lcm/lcm.cpp
+++ 004_Programming_Assignment/lcm/lcm.cpp
@@ -8,9 +8,25 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd_euclid(int a, int b) {
+  if (b == 0)
+    return a;
+  int rem = a % b;
+  return gcd_euclid(b, rem);
+}
+
+/*
+  Calculate LCM using Greatest Common Divisor (GCD)
+  See https://artofproblemsolving.com/wiki/index.php?title=Least_common_multiple#Using_the_GCD
+  LCM(a,b) = a*b / GCD(a,b)
+*/
+long long lcm_gcd(int a, int b) {
+  return (long long) a * b / gcd_euclid(a, b) ;
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_gcd(a, b) << std::endl;
   return 0;
 }
