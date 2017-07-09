4fcbe4b2e4ead9f9b8259d750ed6a20db3f556fa
Seyed Reza Mirkhani <seyedrezamirkhani@yahoo.co.uk>
2017-07-09 23:00:35 +0100

    added implementatino for greatest common divisor using euclid formula

--- 004_Programming_Assignment/gcd/gcd.py
+++ 004_Programming_Assignment/gcd/gcd.py
@@ -10,7 +10,17 @@ def gcd_naive(a, b):
 
     return current_gcd
 
+'''
+gdc_euclid takes two integers where a > b
+returns Greatest Common Divisor
+'''
+def gcd_euclid(a, b):
+  if b == 0:
+     return a
+  rem = a % b # a'    
+  return gcd_euclid(b, rem)
+
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    print(gcd_euclid(a, b))
